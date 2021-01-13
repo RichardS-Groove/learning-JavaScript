74b14434773aea77b01641bef798634be050456e
RichardS <richardsjcb@gmail.com>
2021-01-13 12:56:24 -0300

    Se agregan la clase final de css

---
+++ CSSLEARN/index.js
@@ -0,0 +1,18 @@
+window.load = iniciar();
+
+
+function iniciar() {
+
+    let a, b;
+
+    var respuesta;
+
+    a = 10;
+    b = 2;
+
+    a = 5;
+    respuesta = a * b;
+
+    console.log('A + B es  = ' + respuesta);
+    alert(a + ' + ' + b + ' = ' + respuesta);
+}
